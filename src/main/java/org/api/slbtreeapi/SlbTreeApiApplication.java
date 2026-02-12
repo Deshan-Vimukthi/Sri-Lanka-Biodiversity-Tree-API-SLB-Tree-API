5f7acd79c77fba172a464d4623f67caef3f45087
Deshan.Official <deshan.w@magises.com>
2026-02-13 00:12:46 +0530

    first commit

---
+++ src/main/java/org/api/slbtreeapi/SlbTreeApiApplication.java
@@ -0,0 +1,13 @@
+package org.api.slbtreeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlbTreeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlbTreeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/api/slbtreeapi/SlbTreeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.api.slbtreeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlbTreeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
